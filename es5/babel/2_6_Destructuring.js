696abc257b078cf5710ea2ce0d0ccb7704e508d4
Iulia <iulitaro@gmail.com>
2016-11-22 20:08:20 +0100

    Checkpoint: The destructing option

--- es5/babel/2_6_Destructuring.js
+++ es5/babel/2_6_Destructuring.js
@@ -4,15 +4,15 @@
 
 (function () {
 	var coords = [47.070606, 15.4345208, 359];
-	var lat = coords[1];
-	var len = coords[2];
+	var lat = coords[0];
+	var lon = coords[1];
 	//https://en.wikipedia.org/wiki/Normalnull
 	//Normalnull ("standard zero") or Normal-Null (short N. N. or NN ) is an outdated official vertical datum used in Germany. 
 	//Elevations using this reference system were to be marked "Meter über Normal-Null" (“meters above standard zero”). 
 	//Normalnull has been replaced by Normalhöhennull (short NHN).
-	var normalNull = coords[3];
+	var normalNull = coords[2];
 
-	console.log("lat=" + lat + " lan = " + len + " normalNull = " + normalNull);
+	console.log("lat=" + lat + " lan = " + lon + " normalNull = " + normalNull);
 })();
 
 //ES6
@@ -23,4 +23,6 @@
 	//in this case, we have used a destructing construction
 	//all the three vars are now assembled into an array
 	var [lat, lon, normalNull] = coords;
+
+	console.log("lat=" + lat + " lan = " + lon + " normalNull = " + normalNull);
 })();
--- es5/traceur/2_6_Destructuring.js
+++ es5/traceur/2_6_Destructuring.js
@@ -1,10 +1,10 @@
 "use strict";
 (function() {
   var coords = [47.070606, 15.4345208, 359];
-  var lat = coords[1];
-  var len = coords[2];
-  var normalNull = coords[3];
-  console.log("lat=" + lat + " lan = " + len + " normalNull = " + normalNull);
+  var lat = coords[0];
+  var lon = coords[1];
+  var normalNull = coords[2];
+  console.log("lat=" + lat + " lan = " + lon + " normalNull = " + normalNull);
 }());
 (function() {
   var $__1,
@@ -14,4 +14,5 @@
       lat = ($__1 = $__0[Symbol.iterator](), ($__2 = $__1.next()).done ? void 0 : $__2.value),
       lon = ($__2 = $__1.next()).done ? void 0 : $__2.value,
       normalNull = ($__2 = $__1.next()).done ? void 0 : $__2.value;
+  console.log("lat=" + lat + " lan = " + lon + " normalNull = " + normalNull);
 }());

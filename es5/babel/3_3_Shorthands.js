ad33adb8d33d32b053d269e06a4f13c845c1efb8
Iulia <iulitaro@gmail.com>
2016-11-22 22:25:00 +0100

    Checkpoint: Shorthands for methods and properties

--- es5/babel/3_3_Shorthands.js
+++ es5/babel/3_3_Shorthands.js
@@ -1,84 +1,40 @@
 "use strict";
 
-//ES6
+//ES5 
 
 (function () {
-	class BankAccount {
-		// Changes:
-		// - remove functions, add constructor
-		// - remove ;
-		// - remove return of the BankAccount
-		// - remove the : before the properties
-		//define - constructor
-		constructor(accountNumber, holder, balance) {
-			this._accountNumber = accountNumber;
-			this.accountHolder = holder;
-			this._balance = balance;
+	var lat = 47.070606;
+	var lon = 15.4345208;
+	var normalNull = 359;
+
+	var pos = {
+		lat: lat,
+		lon: lon,
+		normalNull: normalNull
+	};
+	//the position has a method print
+	var posPrinter = {
+		print: function (p) {
+			console.log(`lat=${ pos.lat } lon=${ pos.lon } normalNull=${ pos.normalNull }`);
 		}
-		//define a property balance that returns the current state of the account
-		//there are many lines of code necessary to define properties	
-		get balance() {
-			return this._balance;
+	};
+
+	posPrinter.print(pos);
+})();
+
+//ES6
+(function () {
+	var lat = 47.070606;
+	var lon = 15.4345208;
+	var normalNull = 359;
+
+	var pos = { lat, lon, normalNull };
+	//shorthand for properties and methods
+	var posPrinter = {
+		print(p) {
+			console.log(`lat=${ pos.lat } lon=${ pos.lon } normalNull=${ pos.normalNull }`);
 		}
+	};
 
-		//setter
-		/*
-  set balance(value){
-  	this._balance = value;
-  }*/
-
-		get accountNumber() {
-			return this._accountNumber;
-		}
-
-		//method - deposit
-		deposit(amount) {
-			if (amount < 0) {
-				return;
-			}
-			this._balance += amount;
-		}
-
-		//method - withdraw
-		withdraw(amount) {
-			if (amount < 0 || amount > this._balance) {
-				return;
-			}
-			this._balance -= amount;
-		}
-
-		toString() {
-			return `Account:  ${ this._accountNumber }  holder: ${ this.accountHolder } account state:  ${ this._balance }`;
-		}
-	}
-
-	class ChildBankAccount extends BankAccount {
-		//define a constructor if there are additional parameters
-		constructor(accountNumber, holder, balance, parentName) {
-			super(accountNumber, holder, balance);
-			this.parentName = parentName;
-		}
-
-		withdraw(amount) {
-			if (amount > 50) {
-				return;
-			}
-			super.withdraw(this, amount);
-		}
-
-		toString() {
-			return super.toString() + ` Parent:  ${ this.parentName }`;
-		}
-	}
-
-	var account = new BankAccount(4711, "Wilhelm Brause", 100);
-	account.deposit(200);
-	account.withdraw(50);
-	console.log(account.toString());
-
-	//here Wilhelm Brause is the parent of Peter Brause
-	var childAccount = new ChildBankAccount(4712, "Peter Brause", 30, "Wilhelm Brause");
-	childAccount.deposit(40);
-	childAccount.withdraw(51);
-	console.log(childAccount.toString());
+	posPrinter.print(pos);
 })();

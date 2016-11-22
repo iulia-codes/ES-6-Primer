bf2c277176fd03c3d1f90a9b6a259755f01727f2
Iulia <iulitaro@gmail.com>
2016-11-22 20:03:41 +0100

    Checkpoint: The destructing option

---
+++ es5/traceur/2_6_Destructuring.js
@@ -0,0 +1,17 @@
+"use strict";
+(function() {
+  var coords = [47.070606, 15.4345208, 359];
+  var lat = coords[1];
+  var len = coords[2];
+  var normalNull = coords[3];
+  console.log("lat=" + lat + " lan = " + len + " normalNull = " + normalNull);
+}());
+(function() {
+  var $__1,
+      $__2;
+  var coords = [47.070606, 15.4345208, 359];
+  var $__0 = coords,
+      lat = ($__1 = $__0[Symbol.iterator](), ($__2 = $__1.next()).done ? void 0 : $__2.value),
+      lon = ($__2 = $__1.next()).done ? void 0 : $__2.value,
+      normalNull = ($__2 = $__1.next()).done ? void 0 : $__2.value;
+}());

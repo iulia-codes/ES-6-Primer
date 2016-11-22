8f1a1dc3fa792953946677543c06f948d8df8515
Iulia <iulitaro@gmail.com>
2016-11-22 19:15:35 +0100

    Checkpoint: The Rest parameter

---
+++ es5/babel/2_4_Rest.js
@@ -0,0 +1,43 @@
+"use strict";
+
+//ES5
+
+(function () {
+	function AddNumbers(nr1, nr2) {
+		var result = nr1 + nr2;
+		//arguments[0]	1
+		//arguments[1]	2
+		//arguments[2]	3
+		//arguments[3]	4
+		//arguments[4]	5
+		for (var i = 2; i < arguments.length; i++) {
+			result += arguments[i];
+		}
+		return result;
+	}
+
+	console.log(AddNumbers(1, 2));
+	console.log(AddNumbers(1, 2, 3, 4, 5));
+})();
+
+//ES6
+// define the rest parameter with ...numbers
+// all the extra parameters are in the ...numbers handled, therefore it is no longer necessary 
+// to  use the arguments call
+(function () {
+	function AddNumbers(nr1, nr2, ...numbers) {
+		var result = nr1 + nr2;
+		//arguments[0]	1
+		//arguments[1]	2
+		//arguments[2]	3
+		//arguments[3]	4
+		//arguments[4]	5
+		for (var i = 0; i < numbers.length; i++) {
+			result += arguments[i];
+		}
+		return result;
+	}
+
+	console.log(AddNumbers(1, 2));
+	console.log(AddNumbers(1, 2, 3, 4, 5));
+})();
---
+++ es5/traceur/2_4_Rest.js
@@ -0,0 +1,26 @@
+"use strict";
+(function() {
+  function AddNumbers(nr1, nr2) {
+    var result = nr1 + nr2;
+    for (var i = 2; i < arguments.length; i++) {
+      result += arguments[i];
+    }
+    return result;
+  }
+  console.log(AddNumbers(1, 2));
+  console.log(AddNumbers(1, 2, 3, 4, 5));
+}());
+(function() {
+  function AddNumbers(nr1, nr2) {
+    for (var numbers = [],
+        $__0 = 2; $__0 < arguments.length; $__0++)
+      numbers[$__0 - 2] = arguments[$__0];
+    var result = nr1 + nr2;
+    for (var i = 0; i < numbers.length; i++) {
+      result += arguments[i];
+    }
+    return result;
+  }
+  console.log(AddNumbers(1, 2));
+  console.log(AddNumbers(1, 2, 3, 4, 5));
+}());
